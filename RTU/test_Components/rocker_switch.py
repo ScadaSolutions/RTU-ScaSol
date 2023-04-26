47b9fa49b9292a04d8d31e8acdff290cd094d6f7
ScaSol <115100797+ScadaSolutions@users.noreply.github.com>
2023-04-25 19:02:05 -0500

    Delete RTU directory

--- RTU/test_Components/rocker_switch.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-import time
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-while True:
-	print("Pin status: ", GPIO.input(40))
-GPIO.cleanup()
