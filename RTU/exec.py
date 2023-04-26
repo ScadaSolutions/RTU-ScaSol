47b6d32aaf378e415da944e07b3175bf78f39101
ScaSol <115100797+ScadaSolutions@users.noreply.github.com>
2023-04-25 19:00:59 -0500

    Add files via upload

---
+++ RTU/exec.py
@@ -0,0 +1,110 @@
+import json
+import time
+import RPi.GPIO as GPIO
+import os
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+from servo.servo_functions import *
+from rocker.rocker_functions import *
+from relay.relay_functions import *
+from thermistor.thermistor_functions import *
+from mcp3008.mcp_config import mcp3008Setup
+from prettytable import PrettyTable
+
+
+with open("config_pin.json", "r") as configFile:
+    pinout = json.load(configFile)
+    configFile.close()
+
+servo_pin = pinout.get("servo_pin")
+rocker_sw_pin = pinout.get("rocker_sw_pin")
+relay_input = pinout.get("relay_pin_in")
+relay_output = pinout.get("relay_pin_out")
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+pwm = servoSetup(servo_pin)
+#setAngle(-20, servo_pin, pwm)
+channel_0, channel_1 = mcp3008Setup()
+rockerSetup(rocker_sw_pin)
+relaySetup(relay_input, relay_output)
+
+
+#commands:
+#	chpos "" - change servo position
+#	relay on - relay on
+#	relay off - relay off
+#	pull n - pull data last n rows
+
+data_ls = [["Servo Angle", "Temp C", "temp_F", "Rkr Switch", "Relay"]]
+
+while True:
+
+	with open("output.txt", "r") as cmd_file:
+		cmd = cmd_file.read()
+		cmd_file.close()
+
+	if cmd == "":
+		#NO CMD [!]
+		pass
+	elif cmd =="exit":
+		break
+	else:
+		cmd = cmd.split()
+
+		if cmd[0] == "chpos":
+			try:
+				setAngle(int(cmd[1]), servo_pin, pwm)
+			except ValueError:
+				print("Invalid Servo CMD")
+		elif cmd[0] == "relay":
+			if cmd[1] == "on":
+				#
+				relayOn(relay_output)
+			elif cmd[1] == "off":
+				#
+				relayOff(relay_output)
+		elif cmd[0] == "pull":
+
+			if len(cmd) == 1:
+
+				print()
+				table = PrettyTable(data_ls[0])
+				for data in data_ls[1:]:
+					table.add_row(data)
+				print(table)
+				print()
+			else:
+				try:
+					n = int(cmd[1])
+					new_data = data_ls[-n-1:]
+					table = PrettyTable(data_ls[0])
+					for data in new_data[1:]:
+						table.add_row(data)
+					print()
+					print(table)
+					print()
+				except ValueError:
+					print("\nCMD: 'pull n' -> where n is an integer.\n")
+		else:
+			print("\nInvalid CMD [!]\n")
+
+	servo_adc, servo_vol = reqPos(channel_0)
+	angle_servo = round(calculateAngle(servo_vol), 2)
+
+	therm_adc, therm_vol = reqTemp(channel_1)
+	temp_C, temp_F = calculateTemp(therm_vol)
+
+	rocker_sw = int(swStat(rocker_sw_pin))
+
+	relay = int(relayStat(relay_input))
+
+	ls = [angle_servo, round(temp_C, 2), round(temp_F, 2), rocker_sw, relay]
+	data_ls.append(ls)
+	time.sleep(1.5)
+
+GPIO.cleanup()
---
+++ RTU/servo/servo_functions.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+
+def servoSetup(pin):
+	GPIO.setup(pin, GPIO.OUT)
+	servo  = GPIO.PWM(pin, 50)
+	servo.start(0)
+
+	time.sleep(0.5)
+	return servo
+
+def setAngle(angle, pin, servo):
+	duty = angle / 18 + 2
+	GPIO.output(pin, True)
+	servo.ChangeDutyCycle(duty)
+	time.sleep(2)
+	GPIO.output(pin, False)
+	servo.ChangeDutyCycle(0)
+
+def reqPos(channel):
+	return channel.value, channel.voltage
+
+def calculateAngle(voltage):
+	high_voltage = 2.456
+	min_voltage = 0.416
+	max_angle = 181
+	slope = (high_voltage - min_voltage) / (max_angle)
+	feedback_angle = (voltage - min_voltage) / slope
+	return feedback_angle
